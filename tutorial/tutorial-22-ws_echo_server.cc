b2371d7fbef3ad3451f5804d6867701c1a5be8ef
gnblao <gnblao@gmail.com>
2022-05-07 17:27:15 +0800

    channel: wss server

--- tutorial/tutorial-22-ws_echo_server.cc
+++ tutorial/tutorial-22-ws_echo_server.cc
@@ -25,18 +25,33 @@ void sig_handler(int signo)
 int main(int argc, char *argv[])
 {
 	unsigned short port;
+    char *cert_file;
+    char *key_file;
+    int ret;
 
-	if (argc != 2)
+	if (argc != 2 && argc != 4)
 	{
 		fprintf(stderr, "USAGE: %s <port>\n", argv[0]);
+		fprintf(stderr, "ssl : %s <port> <cert_file> <key_file>\n", argv[0]);
 		exit(1);
 	}
-
+    
+	port = atoi(argv[1]);
+    
 	signal(SIGINT, sig_handler);
 
 	WFWebSocketServer server;
-	port = atoi(argv[1]);
-	if (server.start(port) == 0)
+    
+    if (argc == 4) {
+        cert_file = argv[2];
+        key_file = argv[3];
+    
+        ret = server.start(port, cert_file, key_file);
+    } else {
+	    ret = server.start(port);
+    }
+
+    if (ret== 0)
 	{
 		wait_group.wait();
 		server.stop();
