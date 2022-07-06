fcf120a8daaddc766dadd353eb8c263a0eb46fec
gnblao <gnblao@gmail.com>
2022-07-06 14:51:43 +0800

    channel: change code format

--- src/factory/WFChannel.h
+++ src/factory/WFChannel.h
@@ -1,7 +1,7 @@
 /*************************************************************************
     > File Name: WFChannelMsg.h
-    > Author: gnblao 
-    > Mail: gnblao 
+    > Author: gnblao
+    > Mail: gnblao
     > Created Time: 2022年04月17日 星期日 14时01分32秒
  ************************************************************************/
 
@@ -12,74 +12,83 @@
 #include <cassert>
 #include <functional>
 #include <iostream>
-#include <mutex>
-#include <type_traits>
-#include <queue>
 #include <list>
+#include <mutex>
+#include <queue>
+#include <type_traits>
 #include <utility>
 
 #include "CommScheduler.h"
 #include "Communicator.h"
+#include "ProtocolMessage.h"
 #include "WFTask.h"
 #include "WFTaskFactory.h"
-#include "ProtocolMessage.h"
 
-enum {
-        WFC_MSG_STATE_ERROR = -1,
-        WFC_MSG_STATE_UNDEFINED,
-        WFC_MSG_STATE_IN,
-        WFC_MSG_STATE_OUT,
-        WFC_MSG_STATE_OUT_LIST,
+enum
+{
+    WFC_MSG_STATE_ERROR = -1,
+    WFC_MSG_STATE_UNDEFINED,
+    WFC_MSG_STATE_IN,
+    WFC_MSG_STATE_OUT,
+    WFC_MSG_STATE_OUT_LIST,
 };
 
-class MsgSession :public CommSession {
+class MsgSession : public CommSession
+{
 public:
-    virtual protocol::ProtocolMessage *get_msg() = 0;
-    virtual int get_state() const = 0;
-    virtual void set_state(int state) = 0;
+    virtual protocol::ProtocolMessage *get_msg()            = 0;
+    virtual int                        get_state() const    = 0;
+    virtual void                       set_state(int state) = 0;
 
-    MsgSession(): CommSession() {}
+    MsgSession() : CommSession()
+    {
+    }
     virtual ~MsgSession(){};
 };
 
-class WFChannel {
+class WFChannel
+{
 protected:
     using MSG = protocol::ProtocolMessage;
+
 public:
     using BaseTask = WFNetworkTask<MSG, MSG>;
+
 public:
     virtual MsgSession *new_msg_session() = 0;
 
     virtual void incref() = 0;
     virtual void decref() = 0;
-    
+
     virtual long long get_msg_seq() = 0; /*for in msg only*/
     virtual long long get_req_seq() = 0; /*for req only*/
-    
-    virtual int fanout_msg_in(MSG *in, long long seq) = 0;
+
+    virtual int fanout_msg_in(MSG *in, long long seq)   = 0;
     virtual int fanout_msg_out(MSG *out, long long seq) = 0;
-    virtual int msg_out(MSG *out) = 0;
-    virtual int msg_out_list(MSG *out) = 0;
-    
+    virtual int msg_out(MSG *out)                       = 0;
+    virtual int msg_out_list(MSG *out)                  = 0;
+
     virtual bool is_server() = 0;
-    virtual bool is_open() = 0;
-    virtual int shutdown() = 0;
+    virtual bool is_open()   = 0;
+    virtual int  shutdown()  = 0;
 };
 
-template<typename ChannelBase = WFNetworkTask<protocol::ProtocolMessage, protocol::ProtocolMessage>>
+template <typename ChannelBase =
+              WFNetworkTask<protocol::ProtocolMessage, protocol::ProtocolMessage>>
 class WFChannelImpl : public ChannelBase, public WFChannel
 {
 private:
-    static_assert(std::is_base_of<BaseTask, ChannelBase>::value, 
-            "WFNetworkTask<protocol::ProtocolMessage, protocol::ProtocolMessage>> must is base of ChannelBase");
+    static_assert(std::is_base_of<BaseTask, ChannelBase>::value,
+                  "WFNetworkTask<protocol::ProtocolMessage, protocol::ProtocolMessage>> must is "
+                  "base of ChannelBase");
 
 protected:
-	using task_callback_t = std::function<void (BaseTask *)>;
-	
+    using task_callback_t = std::function<void(BaseTask *)>;
+
     virtual CommMessageIn *message_in()
     {
         MsgSession *session;
-        
+
         if (!this->is_open())
             return nullptr;
 
@@ -88,18 +97,20 @@ protected:
             return msg;
 
         session = this->new_msg_session();
-        if (!session) {
+        if (!session)
+        {
             return nullptr;
         }
 
         msg = session->get_msg();
         session->set_state(WFC_MSG_STATE_IN);
 
-        if (!msg) {
+        if (!msg)
+        {
             return nullptr;
         }
 
-        msg->seq = this->msg_seq++;
+        msg->seq     = this->msg_seq++;
         msg->session = session;
         return msg;
     }
@@ -107,12 +118,13 @@ protected:
     virtual CommMessageOut *message_out()
     {
         CommMessageOut *msg;
-        
-        //if (!this->is_open())
-        //    return nullptr;
+
+        // if (!this->is_open())
+        //     return nullptr;
 
         std::lock_guard<std::mutex> lck(this->write_mutex);
-        if (this->write_list.size()) {
+        if (this->write_list.size())
+        {
             msg = this->write_list.front();
             this->write_list.pop_front();
             return msg;
@@ -121,92 +133,117 @@ protected:
         return nullptr;
     }
 
-
 private:
     std::atomic<long long> msg_seq;
     std::atomic<long long> req_seq;
-    std::atomic<long> ref;
-    
+    std::atomic<long>      ref;
+
     std::atomic_bool stop_flag{false};
 
 public:
-    //virtual MsgSession *new_msg_session() {return nullptr;};
-    long long get_msg_seq() {return this->msg_seq;}
-    long long get_req_seq() {return this->req_seq;}
-	
-    virtual int shutdown() {
+    // virtual MsgSession *new_msg_session() {return nullptr;};
+    long long get_msg_seq()
+    {
+        return this->msg_seq;
+    }
+
+    long long get_req_seq()
+    {
+        return this->req_seq;
+    }
+
+    virtual int shutdown()
+    {
         this->stop_flag.exchange(true);
         this->get_scheduler()->channel_shutdown(this);
         return 0;
     }
-    
-    virtual bool is_open() {return !this->stop_flag;};
 
-    virtual void incref() {
+    virtual bool is_open()
+    {
+        return !this->stop_flag;
+    };
+
+    virtual void incref()
+    {
         this->ref++;
     }
-    virtual void decref() {
+    virtual void decref()
+    {
         if (--this->ref == 0)
             delete this;
     }
-    
-    virtual bool is_server() {return false;}
-    virtual bool is_channel() {return true;}
+
+    virtual bool is_server()
+    {
+        return false;
+    }
+    virtual bool is_channel()
+    {
+        return true;
+    }
 
 private:
-    using __MSG = std::pair<long long, MSG *>; 
-    class cmp {
+    using __MSG = std::pair<long long, MSG *>;
+    class cmp
+    {
     public:
-        bool operator() (__MSG a, __MSG b) {
-            return a.first > b.first;        
+        bool operator()(__MSG a, __MSG b)
+        {
+            return a.first > b.first;
         }
     };
-    
-    std::mutex in_mutex;
-    std::priority_queue<__MSG, std::vector<__MSG>, cmp> fanout_heap_in;
-    std::list<MSG*> in_list;
-    long long in_list_seq = 0;
 
-    std::mutex out_mutex;
-    std::priority_queue<__MSG, std::vector<__MSG>, cmp> fanout_heap_out;
-    std::list<MSG*> out_list;
-    long long out_list_seq = 0;
-    
-    std::mutex write_mutex;
-    std::list<MSG*> write_list;
-    
+    using __MSG_HEAP = std::priority_queue<__MSG, std::vector<__MSG>, cmp>;
+
+    std::mutex       in_mutex;
+    __MSG_HEAP       fanout_heap_in;
+    std::list<MSG *> in_list;
+    long long        in_list_seq = 0;
+
+    std::mutex       out_mutex;
+    __MSG_HEAP       fanout_heap_out;
+    std::list<MSG *> out_list;
+    long long        out_list_seq = 0;
+
+    std::mutex       write_mutex;
+    std::list<MSG *> write_list;
+
 public:
-    virtual int process_msg(MSG *msg) {
+    virtual int process_msg(MSG *msg)
+    {
         return 0;
     }
-    
+
     virtual int fanout_msg_in(MSG *in, long long seq)
-	{
-        int ret;
+    {
+        int          ret;
         CommSession *cur_session = in->session;
 
         if (!this->is_open())
             return -1;
-        
+
         std::lock_guard<std::mutex> lck(this->in_mutex);
         assert(this->in_list_seq <= seq);
 
         fanout_heap_in.emplace(std::make_pair(seq, in));
-        while (fanout_heap_in.top().first == this->in_list_seq) {
-            auto x =  fanout_heap_in.top();
+        while (fanout_heap_in.top().first == this->in_list_seq)
+        {
+            auto x = fanout_heap_in.top();
             fanout_heap_in.pop();
-            this->in_list_seq ++;
+            this->in_list_seq++;
 
             if (x.second)
                 in_list.push_back(x.second);
         }
-       
-        while (in_list.size()) {
+
+        while (in_list.size())
+        {
             MSG *msg = in_list.front();
             in_list.pop_front();
-            
+
             msg->session = cur_session;
-            ret = process_msg(msg);
+            ret          = process_msg(msg);
             delete msg;
             if (ret < 0)
                 break;
@@ -223,35 +260,38 @@ public:
 
         std::lock_guard<std::mutex> lck(this->out_mutex);
         assert(this->out_list_seq <= seq);
-        
+
         fanout_heap_out.emplace(std::make_pair(seq, out));
-        while (fanout_heap_out.top().first == this->out_list_seq) {
-            auto x =  fanout_heap_out.top();
+        while (fanout_heap_out.top().first == this->out_list_seq)
+        {
+            auto x = fanout_heap_out.top();
             fanout_heap_out.pop();
-            
-            this->out_list_seq ++;
+
+            this->out_list_seq++;
             if (x.second)
                 out_list.push_back(x.second);
         }
-       
-        while (out_list.size()) {
+
+        while (out_list.size())
+        {
             MSG *msg = out_list.front();
             out_list.pop_front();
 
             ret = this->msg_out(msg);
-            if (ret < 0) {
+            if (ret < 0)
+            {
                 delete msg;
                 break;
             }
         }
-        
+
         return 0;
     }
-    
+
     virtual int msg_out(MSG *out, int flag)
     {
         int ret;
-        
+
         if (!this->is_open())
             return -1;
 
@@ -259,109 +299,121 @@ public:
             std::lock_guard<std::mutex> lck(this->write_mutex);
             this->write_list.push_back(out);
         }
-        
+
         if (flag == WFC_MSG_STATE_OUT_LIST)
             return 0;
 
         ret = this->get_scheduler()->channel_send_one(this);
-        if (ret < 0) { 
+        if (ret < 0)
+        {
             this->shutdown();
         }
 
         return 0;
     }
 
-    virtual int msg_out(MSG *out) {
+    virtual int msg_out(MSG *out)
+    {
         return this->msg_out(out, WFC_MSG_STATE_OUT);
     }
-    virtual int msg_out_list(MSG *out) {
+    virtual int msg_out_list(MSG *out)
+    {
         return this->msg_out(out, WFC_MSG_STATE_OUT_LIST);
     }
-protected:
-	/*for client*/
-	explicit WFChannelImpl(int retry_max, task_callback_t&& cb):
-	    ChannelBase(retry_max, std::move(cb))
-	{
-        this->msg_seq = 0;
-        this->req_seq = 0;
-        this->ref = 1;
-        this->stop_flag = false;
-	}
-	
-    /*for server*/
-	explicit WFChannelImpl(CommScheduler *scheduler, task_callback_t&& cb):
-	    ChannelBase(nullptr, scheduler, std::move(cb))
-	{
-        this->msg_seq = 0;
-        this->req_seq = 0;
-        this->ref = 1;
-        this->stop_flag = false;
-	}
 
 protected:
-    void delete_this(void *t) {
+    /*for client*/
+    explicit WFChannelImpl(int retry_max, task_callback_t &&cb)
+        : ChannelBase(retry_max, std::move(cb))
+    {
+        this->msg_seq   = 0;
+        this->req_seq   = 0;
+        this->ref       = 1;
+        this->stop_flag = false;
+    }
+
+    /*for server*/
+    explicit WFChannelImpl(CommScheduler *scheduler, task_callback_t &&cb)
+        : ChannelBase(nullptr, scheduler, std::move(cb))
+    {
+        this->msg_seq   = 0;
+        this->req_seq   = 0;
+        this->ref       = 1;
+        this->stop_flag = false;
+    }
+
+protected:
+    virtual void delete_this(void *t)
+    {
         this->stop_flag = true;
-        
+
         CommMessageIn *in = this->get_message_in();
-        if (in) {
-            if (in->session) {
+        if (in)
+        {
+            if (in->session)
+            {
                 delete in->session;
                 in->session = nullptr;
             }
-            //delete in;
+            // delete in;
         }
-        
+
         this->decref();
     }
 
     virtual ~WFChannelImpl()
-    { 
-        while (!fanout_heap_in.empty()) {
+    {
+        while (!fanout_heap_in.empty())
+        {
             auto _msg = fanout_heap_in.top();
             fanout_heap_in.pop();
 
             delete _msg.second;
         }
 
-        for (auto x : in_list) {
+        for (auto x : in_list)
+        {
             in_list.remove(x);
             delete x;
         }
 
-        while (!fanout_heap_out.empty()) {
+        while (!fanout_heap_out.empty())
+        {
             auto _msg = fanout_heap_out.top();
             fanout_heap_out.pop();
 
             delete _msg.second;
         }
 
-        for (auto x : out_list) {
+        for (auto x : out_list)
+        {
             out_list.remove(x);
             delete x;
         }
 
-        for (auto x : write_list) {
+        for (auto x : write_list)
+        {
             write_list.remove(x);
             delete x;
         }
-        
-        //CommMessageIn *in = this->get_message_in();
-        //if (in) {
-        //    if (in->session) {
-        //        delete in->session;
-        //        in->session = nullptr;
-        //    }
-        //    //delete in;
-        //    *(this->get_in()) = nullptr;
-        //}
-        
+
+        // CommMessageIn *in = this->get_message_in();
+        // if (in) {
+        //     if (in->session) {
+        //         delete in->session;
+        //         in->session = nullptr;
+        //     }
+        //     //delete in;
+        //     *(this->get_in()) = nullptr;
+        // }
+
         CommMessageOut *out = this->get_message_out();
-        if (out) {
+        if (out)
+        {
             delete out;
             *(this->get_out()) = nullptr;
         }
     }
-
 };
 
 #include "WFChannel.inl"
--- src/factory/WFChannel.inl
+++ src/factory/WFChannel.inl
@@ -5,76 +5,88 @@
     > Created Time: 2022年05月05日 星期四 11时37分35秒
  ************************************************************************/
 
-
-using WFChannelClientBase = WFChannelImpl<WFComplexClientTask<protocol::ProtocolMessage, protocol::ProtocolMessage>>;
-using WFChannelServerBase = WFChannelImpl<WFNetworkTask<protocol::ProtocolMessage, protocol::ProtocolMessage>>;
+using WFChannelClientBase =
+    WFChannelImpl<WFComplexClientTask<protocol::ProtocolMessage, protocol::ProtocolMessage>>;
+using WFChannelServerBase =
+    WFChannelImpl<WFNetworkTask<protocol::ProtocolMessage, protocol::ProtocolMessage>>;
 
 class WFChannelClient : public WFChannelClientBase
 {
 protected:
-	virtual CommMessageOut *message_out()
-	{
-		/* By using prepare function, users can modify request after
-		 * the connection is established. */
-		if (this->prepare)
-			this->prepare(this);
+    virtual CommMessageOut *message_out()
+    {
+        /* By using prepare function, users can modify request after
+         * the connection is established. */
+        if (this->prepare)
+            this->prepare(this);
 
-		return WFChannelClientBase::message_out();
-	}
+        return WFChannelClientBase::message_out();
+    }
 
-    virtual int first_timeout() {return this->receive_timeout();}
+    virtual int first_timeout()
+    {
+        return this->receive_timeout();
+    }
 
 private:
-    std::function<void (WFChannel *)> prepare;
+    std::function<void(WFChannel *)> prepare;
 
 public:
-	void set_prepare(std::function<void (WFChannel*)> prep)
-	{
-		this->prepare = std::move(prep);
-	}
-    
+    void set_prepare(std::function<void(WFChannel *)> prep)
+    {
+        this->prepare = std::move(prep);
+    }
+
 public:
-	WFChannelClient(int retry_max, task_callback_t&& cb): 
-        WFChannelClientBase(retry_max, std::move(cb)) {}
+    WFChannelClient(int retry_max, task_callback_t &&cb)
+        : WFChannelClientBase(retry_max, std::move(cb))
+    {
+    }
 
 protected:
-	virtual ~WFChannelClient() {};
+    virtual ~WFChannelClient(){};
 };
 
 class WFChannelServer : public WFChannelServerBase
 {
 protected:
-	virtual void dispatch()
-	{
+    virtual void dispatch()
+    {
         this->subtask_done();
-	}
+    }
 
-    virtual void handle(int state, int error) {
+    virtual void handle(int state, int error)
+    {
         this->start();
     }
-    
-    virtual SubTask *done()
-	{
-        SeriesWork *series = series_of(this);
-		if (this->callback)
-			this->callback(this);
 
-        this->delete_this(this);
-		return series->pop();
-	}
-    
+    virtual SubTask *done()
+    {
+        SeriesWork *series = series_of(this);
+        if (this->callback)
+            this->callback(this);
+
+        this->delete_this(static_cast<void *>(this));
+        return series->pop();
+    }
 
     virtual WFConnection *get_connection() const
-	{
+    {
         return (WFConnection *)this->CommSession::get_connection();
-	}
+    }
 
 public:
-    virtual bool is_server() {return true;}
-	
-    explicit WFChannelServer(CommScheduler *scheduler, CommService *service=nullptr, task_callback_t &&cb = nullptr):
-        WFChannelServerBase(scheduler, std::move(cb)) {}
+    virtual bool is_server()
+    {
+        return true;
+    }
 
-    virtual ~WFChannelServer() {};
+    explicit WFChannelServer(CommScheduler *scheduler, CommService *service = nullptr,
+                             task_callback_t &&cb = nullptr)
+        : WFChannelServerBase(scheduler, std::move(cb))
+    {
+    }
+
+    virtual ~WFChannelServer(){};
 };
 
